e6b242ec5e356e3162315a1a5371d6bdc7d55c13
Lama02 <phuclam261002@gmail.com>
2023-01-13 19:25:48 +0700

    HelloWorld

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#define MAX_LEN 10
+int main()
+{
+	FILE *ptr = fopen("floppy.img","r");
+	uint8_t a[10];
+	fgets(a,10,ptr);
+	printf("%s",a);
+}
